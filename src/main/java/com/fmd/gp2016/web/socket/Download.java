d2d3c4f976d979fe241f510faa37a860373743ad
ibrahimali123 <ibrahim.ali.0403@gmail.com>
2016-03-14 21:56:09 +0200

    map js

--- src/main/java/com/fmd/gp2016/web/socket/Download.java
+++ src/main/java/com/fmd/gp2016/web/socket/Download.java
@@ -14,7 +14,7 @@ public class Download implements Runnable {
 
 	public Download(String saveTo, Socket socket) {
 		try {
-			//System.out.println("Download constract");
+			// System.out.println("Download constract");
 			In = socket.getInputStream();
 			this.saveTo = saveTo;
 		} catch (IOException ex) {
@@ -24,18 +24,20 @@ public class Download implements Runnable {
 
 	@Override
 	public void run() {
+		int p = 0;
 		try {
-			//System.out.println("Download run " + saveTo);
+			// System.out.println("Download run " + saveTo);
 			Out = new FileOutputStream(saveTo);
 
 			byte[] buffer = new byte[1024];
-			int count;
+			int count ;
 
 			while ((count = In.read(buffer)) >= 0) {
-				//System.out.println("Download " + count);
+				// System.out.println("Download " + count);
 				Out.write(buffer, 0, count);
+				p++;
 			}
-
+			
 			Out.flush();
 
 			// sui.jTextArea1.append("[Application > Me] : Download
@@ -47,10 +49,11 @@ public class Download implements Runnable {
 			if (In != null) {
 				In.close();
 			}
-			
+
 		} catch (Exception ex) {
 			System.out.println("Exception [Download : run(...)]");
 			ex.printStackTrace();
 		}
+		System.out.println("p " + p);
 	}
 }
